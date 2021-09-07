8b97442f86261bfa0528bf18d73308e5e08c7925
Lunkaz <marialbornozapata@gmail.com>
2021-09-06 22:02:42 -0300

    Two first cards & if logic

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Guapo")
+let firstCard = Math.floor(Math.random() * (12 - 2) + 2);
+let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂");
+} else if (sum === 21) {
+    console.log("Yeaaah! You've got Blackjack! 🥳");
+    hasBlackJack = true;
+} else {
+    isAlive = false;
+    console.log("You're out of the game! 😭");
+}
+ 
+
+console.log(hasBlackJack);
