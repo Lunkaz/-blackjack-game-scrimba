794c35398b37818a1bc7bd5680aefcbdef1b4e62
Lunkaz <marialbornozapata@gmail.com>
2021-09-08 22:52:48 -0300

    Adding bg image & start button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const startBtn = document.getElementById("start-btn");
+
 let firstCard = Math.floor(Math.random() * (12 - 2) + 2);
 let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
 let sum = firstCard + secondCard;
@@ -5,15 +7,22 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Yeaaah! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    isAlive = false;
-    message ="You're out of the game! 😭";
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Yeaaah! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        isAlive = false;
+        message ="You're out of the game! 😭";
+    }
+
+    console.log(message);
 }
+
+startBtn.addEventListener("click", startGame)
  
 
-console.log(message);
