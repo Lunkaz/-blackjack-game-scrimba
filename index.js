3fada8074be80c1bd9793d55e67b91a9490b4cc6
Lunkaz <marialbornozapata@gmail.com>
2021-09-13 18:30:00 -0300

    cards array

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const newBtn = document.getElementById("new-btn");
 
 let firstCard = Math.floor(Math.random() * (12 - 2) + 2);
 let secondCard = Math.floor(Math.random() * (12 - 2) + 2);
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-let carsd = [];
 
 function startGame() {
     renderGame();
@@ -18,7 +18,7 @@ function startGame() {
 
 function renderGame() {
     sumEl.textContent = `Sum: ${sum}`;
-    cardsEl.textContent = `Cards: ${firstCard} + ${secondCard}`;
+    cardsEl.textContent = `Cards: ${cards[0]} + ${cards[1]}`;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -35,6 +35,7 @@ function renderGame() {
 function newCard() {
     let newCard = Math.floor(Math.random() * (12 - 2) + 2);
     sum +=  newCard;
+    cards.push(newCard);
     renderGame();
 }
 
